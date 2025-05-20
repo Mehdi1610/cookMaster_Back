cb18a34bbac370cc548ee48daf881551d7516f6b
i16nagi <i16.chebbah@gmail.com>
2025-05-20 22:41:29 +0200

    init Project

---
+++ src/main/java/com/cookMaster/cookMaster_back/CookMasterBackApplication.java
@@ -0,0 +1,13 @@
+package com.cookMaster.cookMaster_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookMasterBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookMasterBackApplication.class, args);
+	}
+
+}
